d69a94489ba2c5301b49842f207638be7e647279
Laflow3r <32850330+Laflow3r@users.noreply.github.com>
2018-03-20 20:27:18 -0400

    Launcher1.1

---
+++ PIXEL-WARRIORS/Test 1 - Copie/Test 1 - Copie/Assets/Scripts/Launcher.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+
+
+namespace Com.INDIEN.PixelWarriors
+
+//CODE FROM https://doc.photonengine.com/en-us/pun/current/demos-and-tutorials/pun-basics-tutorial/lobby 
+
+
+{
+    public class Launcher : Photon.PunBehaviour
+    {
+        #region Public Variables
+
+
+        #endregion
+
+
+        #region Private Variables
+
+
+        /// <summary>
+        /// This client's version number. Users are separated from each other by gameversion (which allows you to make breaking changes).
+        /// </summary>
+        string _gameVersion = "1";
+
+
+        #endregion
+
+
+        #region MonoBehaviour CallBacks
+
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
+        /// </summary>
+        void Awake()
+        {
+
+
+            // #Critical
+            // we don't join the lobby. There is no need to join a lobby to get the list of rooms.
+            PhotonNetwork.autoJoinLobby = false;
+
+
+            // #Critical
+            // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
+            PhotonNetwork.automaticallySyncScene = true;
+        }
+
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during initialization phase.
+        /// </summary>
+        void Start()
+        {
+            Connect();
+        }
+
+
+        #endregion
+
+
+        #region Public Methods
+
+
+        /// <summary>
+        /// Start the connection process. 
+        /// - If already connected, we attempt joining a random room
+        /// - if not yet connected, Connect this application instance to Photon Cloud Network
+        /// </summary>
+        public void Connect()
+        {
+
+
+            // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+            if (PhotonNetwork.connected)
+            {
+                // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnPhotonRandomJoinFailed() and we'll create one.
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                // #Critical, we must first and foremost connect to Photon Online Server.
+                PhotonNetwork.ConnectUsingSettings(_gameVersion);
+            }
+        }
+
+
+        #endregion
+
+        #region Photon.PunBehaviour CallBacks
+
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("DemoAnimator/Launcher: OnConnectedToMaster() was called by PUN");
+        }
+
+
+        public override void OnDisconnectedFromPhoton()
+        {
+            Debug.LogWarning("DemoAnimator/Launcher: OnDisconnectedFromPhoton() was called by PUN");
+        }
+
+        public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
+        {
+            Debug.Log("DemoAnimator/Launcher:OnPhotonRandomJoinFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom(null, new RoomOptions() {maxPlayers = 2}, null);");
+            // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+            PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = 2 }, null);
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("DemoAnimator/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+        }
+
+
+        #endregion
+
+    }
+
+
+}
