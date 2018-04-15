aa5b7136d1794c6b15c91040e1ae175b1698ea6e
willgendreau <36709240+willgendreau@users.noreply.github.com>
2018-04-14 20:20:37 -0400

    menu

--- PIXEL-WARRIORS/Test 1 - Copie/Test 1 - Copie/Assets/Scripts/MainMenu.cs
+++ PIXEL-WARRIORS/Test 1 - Copie/Test 1 - Copie/Assets/Scripts/MainMenu.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEditor.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-	public void playButton()
+    public Toggle toggleMap1;
+    public Toggle toggleMap2;
+
+    public void playButton()
     {
-        EditorSceneManager.LoadScene("Lava_Map");
+        if (toggleMap1.isOn)
+        {
+            EditorSceneManager.LoadScene("MAP1");
+        }
+        else if (toggleMap2.isOn)
+        {
+            EditorSceneManager.LoadScene("MAP2");
+        }
     }
 }
